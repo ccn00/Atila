eb8e5f0aa9af570995b2cb6a4d2a037282825acd
Juan Carlos Castro <59028123+ccn00@users.noreply.github.com>
2022-11-11 17:59:02 +0100

    Creado metodo para escanear puertos con threads

--- Escaner.py
+++ Escaner.py
@@ -1,6 +1,6 @@
 import scapy.all as scapy
 import socket
-import thread 
+import threading
 
 class Escaner:
     # # El usuario introduce la ip de la red que quiere escanear o el rango de ips
@@ -13,7 +13,7 @@ class Escaner:
     #                                        METODOS PARA ESCANEO                                                #
     ############################################################################################################
 
-    def check_ip(ip):
+    def check_ip(self, ip):
         # Comprobamos que la ip introducida es valida
         if "." in ip:
             # Separamos la ip en una lista de octetos
@@ -44,49 +44,69 @@ class Escaner:
         # Enviamos el paquete y recibimos la respuesta
         answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]   
 
-        for element in answered_list:                      # Recorremos la lista de clientes encontrados
-            # Creamos un diccionario con la ip y la mac de cada cliente
-            client_dict = {"ip": element[1].psrc, "mac": element[1].hwsrc} 
-            if client_dict not in self.clients_list: 
-                self.clients_list.append(client_dict)
-        return self.clients_list
+        # Recorremos la lista de clientes encontrados
+        for element in answered_list:
+            client_dict = {"ip": element[1].psrc, "mac": element[1].hwsrc}
+            # Incluir a la lista si no esta ya
+            if client_dict not in self.client_list:
+                self.client_list.append(client_dict)
+
+        if len(self.client_list) == 0:
+            print("No se han encontrado clientes")
+        else:
+            # Mostramos los clientes encontrados en forma de tabla
+            print("IP\t\t\tMAC Address")
+            for client in self.client_list:
+                print(client["ip"] + "\t\t" + client["mac"])
+
+        return self.client_list
 
     # Escanea el rango de ips de la red
     def scan_ip_range(self):
         # Mandamos un scan de ips con el rango de ips en base a la tarjeta de red
         scan_result = self.scan_ip(self.get_ip_range())
-        self.print_result_ip(scan_result)
+        self.print_result_ip(self, scan_result)
+
+
+    # Para mejorar el rendimiento del escaneo de puertos lo realizamos en paralelo con hilos
+    def scan_ports_thread(self, ip):
+        print("\nEscaneando puertos de la ip: " + ip)
+        print ("PORT\t\tSTATE\t\tSERVICE")
+        # Creamos un hilo para cada puerto
+        for puerto in range(1, 1025):
+            t = threading.Thread(target=self.scan_ports, args=(ip, puerto))
+            t.start()
 
 
     # Escaner de puertos para una ip
-    def scan_ports(ip):
-        print("Escaneando puertos de la ip: " + ip)
-        print ("PORT\t\tSTATE\t\tSERVICE")
-        for puerto in range(1, 1024):
-            # RandShort() genera un puerto aleatorio para el scan por tanto el escaneo se realizara cada vez con un puerto diferente
-            origin_port = scapy.RandShort()
-            # Creamos un paquete TCP con la ip introducida por el usuario y el puerto que queremos escanear
-            packet = scapy.sr1(scapy.IP(dst=ip)/scapy.TCP(sport=origin_port, dport=puerto, flags="S"), timeout=0.0005, verbose=False)   
-            if packet is not None:
-                # perfcounter_start = time.perf_counter()
-                if packet.haslayer(scapy.TCP):
-                    if packet.getlayer(scapy.TCP).flags == 0x12:
-                        print(str(puerto) + "\t\tOpen\t\t" + socket.getservbyport(puerto))
-                        # Enviamos un paquete RST para cerrar la conexion
-                        scapy.sr1(scapy.IP(dst=ip)/scapy.TCP(sport=origin_port, dport=puerto, flags="R"), timeout=0.05, verbose=False)
-                    # elif packet.getlayer(scapy.TCP).flags == 0x14:
-                    #     print(str(puerto) + "\t\tClosed\t\t" + socket.getservbyport(puerto))
-                elif packet.haslayer(scapy.ICMP):
-                    if int(packet.getlayer(scapy.ICMP).type) == 3 and int(packet.getlayer(scapy.ICMP).code) in [1,2,3,9,10,13]:
-                        print(str(puerto) + "\t\tFiltered\t" + socket.getservbyport(puerto))
-            # perfcounter_end = time.perf_counter() - perfcounter_start
-            # print ("Tiempo de escaneo: " + str(perfcounter_end) + " segundos")
+    def scan_ports(self, ip, puerto):
+        # print("Escaneando puertos de la ip: " + ip)
+        # print ("PORT\t\tSTATE\t\tSERVICE")
+
+        # RandShort() genera un puerto aleatorio para el scan por tanto el escaneo se realizara cada vez con un puerto diferente
+        origin_port = scapy.RandShort()
+        # Creamos un paquete TCP con la ip introducida por el usuario y el puerto que queremos escanear
+        packet = scapy.sr1(scapy.IP(dst=ip)/scapy.TCP(sport=origin_port, dport=puerto, flags="S"), timeout=0.0005, verbose=False)   
+        if packet is not None:
+            # perfcounter_start = time.perf_counter()
+            if packet.haslayer(scapy.TCP):
+                if packet.getlayer(scapy.TCP).flags == 0x12:
+                    print(str(puerto) + "\t\tOpen\t\t" + socket.getservbyport(puerto))
+                    # Enviamos un paquete RST para cerrar la conexion
+                    scapy.sr1(scapy.IP(dst=ip)/scapy.TCP(sport=origin_port, dport=puerto, flags="R"), timeout=0.05, verbose=False)
+                # elif packet.getlayer(scapy.TCP).flags == 0x14:
+                #     print(str(puerto) + "\t\tClosed\t\t" + socket.getservbyport(puerto))
+            # elif packet.haslayer(scapy.ICMP):
+            #     if int(packet.getlayer(scapy.ICMP).type) == 3 and int(packet.getlayer(scapy.ICMP).code) in [1,2,3,9,10,13]:
+            #         print(str(puerto) + "\t\tFiltered\t" + socket.getservbyport(puerto))
+        # perfcounter_end = time.perf_counter() - perfcounter_start
+        # print ("Tiempo de escaneo: " + str(perfcounter_end) + " segundos")
 
             
 
     # Obtenemos la ip de la tarjeta de red
     def get_ip_range(self):
-        # Obtenemos la ip de la tarjeta de red
+        # Obtenemos la ip de la tarjeta de red ethernet 0 (eth0)
         ip = socket.gethostbyname(socket.gethostname())
         # Separamos la ip en octetos
         octetos = ip.split(".")
@@ -94,13 +114,18 @@ class Escaner:
         ip_range = octetos[0] + "." + octetos[1] + "." + octetos[2] + ".1/24"
         return ip_range
 
-    # Escaner de puertos para ips activas en la red
-    def scan_ports_active_ips(self):
-        # Mandamos un scan de ips con el rango de ips en base a la tarjeta de red
-        active_clients = self.scan_ip(self.get_ip_range())
-        print("Escaneando puertos de las ips activas")
-        for client in active_clients:
-            self.scan_ports(client["ip"])
+    # Escaner de puertos de las ips activas encontradas en la red
+    # Pasamos una lista de diccionarios con las ips y macs de los clientes encontrados
+    def scan_ports_active_ips(self, client_list):
+        list_of_ips = []
+        for ip in client_list:
+            self.scan_ports_thread(ip["ip"])
+        #     list_of_ips.append(ip["ip"])
+        # for ip in list_of_ips:
+        #     self.scan
+        
+
+        
 
 
     #############################################################################################################
